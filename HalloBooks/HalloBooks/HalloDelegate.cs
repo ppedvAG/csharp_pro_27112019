f9258d44e9b89d7f3a74d940dd97fb6c094f311f
André <andrer@ppedv.de>
2019-11-27 16:06:50 +0100

    Google Books mit Serialisierung und HalloDelegates

---
+++ HalloBooks/HalloBooks/HalloDelegate.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloBooks
+{
+    delegate void EinfachenDelegate();
+    delegate void DelegateMitPara(string text);
+    delegate long CalcDelegate(int a, int b);
+
+    class HalloDelegate
+    {
+        public HalloDelegate()
+        {
+            EinfachenDelegate meineDele = EinfacheMethode;
+            Action meinDeleAlsAction = EinfacheMethode;
+            Action meinDeleAlsActionAno = delegate () { Console.WriteLine("Hallo"); };
+            Action meinDeleAlsActionAno2 = () => { Console.WriteLine("Hallo"); };
+            Action meinDeleAlsActionAno3 = () => Console.WriteLine("Hallo");
+
+            DelegateMitPara deleMitPara = MethodeMitPara;
+            Action<string> deleMitParaAlsAction = MethodeMitPara;
+            Action<string> deleMitParaAlsActionAno = (string txt) => { Console.WriteLine(txt); };
+            Action<string> deleMitParaAlsActionAno2 = (txt) => Console.WriteLine(txt);
+            Action<string> deleMitParaAlsActionAno3 = x => Console.WriteLine(x);
+
+            CalcDelegate calcDele = Minus;
+            Func<int, int, long> calcDeleAlsFunc = Sum;
+            Func<int, int, long> calcDeleAlsFuncAno = (x, y) => { return x + y; };
+            Func<int, int, long> calcDeleAlsFuncAno2 = (x, y) => x + y;
+
+            List<string> texte = new List<string>();
+
+            texte.Where(FilterNachB);
+            texte.Where(x => x.StartsWith("b"));
+
+            //Predicate<string> == Func<string,bool>
+        }
+
+        private bool FilterNachB(string arg)
+        {
+            if (arg.StartsWith("b"))
+                return true;
+            else
+                return false;
+        }
+
+        private long Minus(int a, int b)
+        {
+            return a - b;
+        }
+
+        private long Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        private void MethodeMitPara(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        private void EinfacheMethode()
+        {
+            Console.WriteLine("Hallo");
+        }
+    }
+}
---
+++ HalloBooks/HalloBooks/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Xml.Serialization;
+
+namespace HalloBooks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Suche(object sender, RoutedEventArgs e)
+        {
+            var url = $"https://www.googleapis.com/books/v1/volumes?q={tb1.Text}";
+
+            using (var web = new WebClient())
+            {
+                var json = web.DownloadString(url);
+                var result = JsonConvert.DeserializeObject<BooksResult>(json);
+                grid.ItemsSource = result.items.Select(x => x.volumeInfo);
+            }
+        }
+
+        private void LinqQuery(object sender, RoutedEventArgs e)
+        {
+            var books = grid.ItemsSource as IEnumerable<Volumeinfo>;
+
+            var query = from b in books
+                        where b.pageCount < 500
+                        orderby b.language, b.title descending
+                        select b;
+
+            grid.ItemsSource = query.ToList();
+        }
+
+        private void LinqLambda(object sender, RoutedEventArgs e)
+        {
+            var books = grid.ItemsSource as IEnumerable<Volumeinfo>;
+
+            grid.ItemsSource = books.Where(b => b.pageCount < 500)
+                                    .OrderBy(x => x.language)
+                                    .ThenByDescending(x => x.title)
+                                    .ToList();
+
+        }
+
+        private void ExportXML(object sender, RoutedEventArgs e)
+        {
+            var books = grid.ItemsSource as IEnumerable<Volumeinfo>;
+            var filename = "book.xml";
+
+            using (var sw = new StreamWriter(filename))
+            {
+                var serial = new XmlSerializer(typeof(List<Volumeinfo>));
+                serial.Serialize(sw, books.ToList());
+                MessageBox.Show("Ok");
+            }
+
+        }
+
+        private void ImportXML(object sender, RoutedEventArgs e)
+        {
+            var filename = "book.xml";
+
+            using (var sr = new StreamReader(filename))
+            {
+                var serial = new XmlSerializer(typeof(List<Volumeinfo>));
+                grid.ItemsSource = (IEnumerable<Volumeinfo>)serial.Deserialize(sr);
+            }
+        }
+    }
+}
