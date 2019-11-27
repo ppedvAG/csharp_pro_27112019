c6cbc4ebe3084ea3f6b4a134dd343849497c2c4f
André <andrer@ppedv.de>
2019-11-27 16:39:49 +0100

    EPPlus + Linq-to-Excel

--- HalloBooks/HalloBooks/MainWindow.xaml.cs
+++ HalloBooks/HalloBooks/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using Newtonsoft.Json;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -77,5 +80,59 @@ namespace HalloBooks
                 grid.ItemsSource = (IEnumerable<Volumeinfo>)serial.Deserialize(sr);
             }
         }
+
+        private void ExportExcel(object sender, RoutedEventArgs e)
+        {
+            var books = grid.ItemsSource as IEnumerable<Volumeinfo>;
+            var filename = "book.xlsx";
+
+            var fi = new FileInfo(filename);
+
+            using (var pack = new ExcelPackage(fi))
+            {
+                var ws = pack.Workbook.Worksheets.FirstOrDefault(x => x.Name == "books");
+                if (ws == null)
+                    ws = pack.Workbook.Worksheets.Add("books");
+
+                ws.Cells[1, 1].Value = "Meine Bücher";
+                int row = 2;
+                foreach (var b in books)
+                {
+                    ws.Cells[row, 1].Value = b.title;
+                    ws.Cells[row, 2].Value = b.subtitle;
+                    ws.Cells[row, 3].Value = b.pageCount;
+                    ws.Cells[row, 4].Value = b.language;
+
+                    if (b.pageCount > 700)
+                    {
+                        ws.Cells[row, 3].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.LightVertical;
+                        ws.Cells[row, 3].Style.Fill.PatternColor.Indexed = 8;
+                    }
+
+                    row++;
+                }
+                pack.Save();
+            }
+            Process.Start(filename);
+        }
+
+        private void SumPages(object sender, RoutedEventArgs e)
+        {
+            var books = grid.ItemsSource as IEnumerable<Volumeinfo>;
+            var filename = "book.xlsx";
+
+            var fi = new FileInfo(filename);
+
+            using (var pack = new ExcelPackage(fi))
+            {
+                var ws = pack.Workbook.Worksheets.FirstOrDefault(x => x.Name == "books");
+
+                var count = ws.Cells["A1:M100"].Where(x => x.Value != null &&
+                                                           x.Style.Fill.PatternType == ExcelFillStyle.LightVertical)
+                                                .Sum(x => int.Parse(x.Value.ToString()));
+                MessageBox.Show($"Count: {count}");
+            }
+        }
     }
 }
+
