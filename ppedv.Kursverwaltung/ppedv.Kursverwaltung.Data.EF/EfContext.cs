e71ce8aa0eb1cd76d78cb0f6c631426deb5ca727
André <andrer@ppedv.de>
2019-11-28 16:31:07 +0100

    Ef für die Kursverwaltung konfiguriert

---
+++ ppedv.Kursverwaltung/ppedv.Kursverwaltung.Data.EF/EfContext.cs
@@ -0,0 +1,18 @@
+﻿using ppedv.Kursverwaltung.Model;
+using System.Data.Entity;
+
+namespace ppedv.Kursverwaltung.Data.EF
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Kurs> Kurse { get; set; }
+        public DbSet<Teilnehmer> Teilnehmer { get; set; }
+        public DbSet<Thema> Themen { get; set; }
+        public DbSet<Trainer> Trainer { get; set; }
+
+        public EfContext(string conString) : base(conString)
+        { }
+        public EfContext() : this("Server=.;Database=Kursverwaltung_dev;Trusted_Connection=true;")
+        { }
+    }
+}
