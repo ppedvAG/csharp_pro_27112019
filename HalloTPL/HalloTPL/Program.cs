5076e2e6d04944ef3a15c2de3a7740c65c4964b7
André <andrer@ppedv.de>
2019-11-28 10:07:05 +0100

    Einfachen Task starten

--- HalloTPL/HalloTPL/Program.cs
+++ HalloTPL/HalloTPL/Program.cs
@@ -11,8 +11,16 @@ namespace HalloTPL
             Console.WriteLine("Hello World!");
 
             //Parallel.Invoke(Zähle, Zähle, Zähle, Zähle);
-            Parallel.For(0, 10000000, i => Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: {i}"));
+            //Parallel.For(0, 10000000, i => Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: {i}"));
 
+            var t1 = new Task(() =>
+            {
+                Console.WriteLine("t1 gestartet");
+                Thread.Sleep(800);
+                Console.WriteLine("t1 fertig");
+            });
+
+            t1.Start();
 
             Console.WriteLine("Ende");
             Console.ReadKey();
