449d37be083417564e5ad59589c412fd3d3da2f8
André <andrer@ppedv.de>
2019-11-28 10:34:19 +0100

    Task mit verschiedenen ContinueWith

--- HalloTPL/HalloTPL/Program.cs
+++ HalloTPL/HalloTPL/Program.cs
@@ -18,7 +18,12 @@ namespace HalloTPL
                 Console.WriteLine("t1 gestartet");
                 Thread.Sleep(800);
                 Console.WriteLine("t1 fertig");
+                throw new NullReferenceException();
             });
+
+            t1.ContinueWith(t => Console.WriteLine("T1c fertig"), TaskContinuationOptions.None);
+            t1.ContinueWith(t => Console.WriteLine($"T1 ERROR {t.Exception.InnerException.Message}"), TaskContinuationOptions.OnlyOnFaulted);
+            t1.ContinueWith(t => Console.WriteLine("T1 OK"), TaskContinuationOptions.OnlyOnRanToCompletion);
 
             var t2 = new Task<long>(() =>
             {
@@ -28,8 +33,17 @@ namespace HalloTPL
                 return 34567890;
             });
 
+            t2.ContinueWith(t =>
+            {
+                Console.WriteLine($"T2 Continue {t.Result}");
+            });
+
             t1.Start();
             t2.Start();
+
+            //t2.Wait();
+            //Task.WaitAll(t1, t2);
+            //Console.WriteLine($"{t2.Result}");
 
             Console.WriteLine("Ende");
             Console.ReadKey();
