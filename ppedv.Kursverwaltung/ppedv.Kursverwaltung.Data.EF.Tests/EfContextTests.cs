d85571845a497f04d82b32753bd7c9fea9ec3d64
André <andrer@ppedv.de>
2019-11-28 17:14:04 +0100

    Tests für Datalayer

---
+++ ppedv.Kursverwaltung/ppedv.Kursverwaltung.Data.EF.Tests/EfContextTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ppedv.Kursverwaltung.Model;
+
+namespace ppedv.Kursverwaltung.Data.EF.Tests
+{
+    [TestClass]
+    public class EfContextTests
+    {
+        [TestMethod]
+        public void Can_create_DB()
+        {
+            using (var con = new EfContext())
+            {
+                if (con.Database.Exists())
+                    con.Database.Delete();
+
+                con.Database.Create();
+
+                Assert.IsTrue(con.Database.Exists());
+            }
+        }
+
+        [TestMethod]
+        public void Can_CRUD_Kurs()
+        {
+            var kurs = new Kurs()
+            {
+                Start = new DateTime(2000, 1, 1),
+                Ort = $"Düsseldoof_{Guid.NewGuid()}"
+            };
+            string newOrt = $"Köln_{Guid.NewGuid()}";
+
+            //INSERT
+            using (var con = new EfContext())
+            {
+                con.Kurse.Add(kurs);
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext())
+            {
+                //READ
+                var loaded = con.Kurse.Find(kurs.Id);
+                Assert.IsNotNull(loaded);
+                Assert.AreEqual(kurs.Ort, loaded.Ort);
+
+                //UPDATE
+                loaded.Ort = newOrt;
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext())
+            {
+                //Test UPDATE
+                var loaded = con.Kurse.Find(kurs.Id);
+                Assert.AreEqual(newOrt, loaded.Ort);
+
+                //DELETE
+                con.Kurse.Remove(loaded);
+                con.SaveChanges();
+            }
+
+            //test DELETE
+            using (var con = new EfContext())
+            {
+                //READ
+                var loaded = con.Kurse.Find(kurs.Id);
+                Assert.IsNull(loaded);
+            }
+        }
+    }
+}
---
+++ ppedv.Kursverwaltung/ppedv.Kursverwaltung.Data.EF.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ppedv.Kursverwaltung.Data.EF.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ppedv.Kursverwaltung.Data.EF.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6cd12055-90f1-4543-8f08-676a614fd99f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
